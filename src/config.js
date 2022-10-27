1cfe5fbe35ab879c48507a105b5d48235e40471e
TAHA <taha.19505@iqra.edu.pk>
2022-10-27 21:00:04 +0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  domainUrl: "growthfountain.com",
+  serverUrl: "https://django-api.growthfountain.com",
+  raiseCapitalServer: "https://api-raisecapital.growthfountain.com",
+  authServer: "https://api-auth.growthfountain.com",
+  formcServer: "https://api-formc.growthfountain.com",
+  investmentServer: "https://api-investment.growthfountain.com",
+  filerServer: "https://api-filer-go.growthfountain.com",
+  esignServer: "https://api-esign-go.growthfountain.com",
+  commentsServer: "https://api-comments.growthfountain.com",
+  blogServer: "https://api-blog.growthfountain.com",
+  notificationsServer: "https://notifications.growthfountain.com/",
+  emailServer: "https://api-email.growthfountain.com",
+  bucketServer: "https://s3.amazonaws.com/growthfountain-alpha-storage",
+
+  teamName: 'Team',
+  teamTitle: "Meet The Team",
+  siteTitle: 'GrowthFountain | Equity Crowdfunding Platform',
+  siteDescription: 'Invest as little as $100 in startups and small businesses. GrowthFountain is the preferred equity crowdfunding platform of more than 1 million credit union members',
+
+  facebookClientId: '1071081646296574',
+  googleClientId: '805823281871-ve3unmva9aer69papghudk0dnpf8tqr2.apps.googleusercontent.com',
+  linkedinClientId: "77ibchutnkhnd7",
+
+  googleMapKey: "AIzaSyBpCl9-7bkVISZ0o-AaFCsKzZwGAxalkZU",
+  stripeKey: "pk_live_4ZF2RPjAVJ0tXrDNMauofmgb",
+
+  //analytics services settings
+  googleTagID: 'GTM-NC9XW5D',
+  googleAnalyticsID: 'UA-47199302-4',
+  googleAnalyticsIDGeneral: 'UA-47199302-1',
+  facebookPixelID: 172026009946228,
+
+	indexPage: {
+    //main slider or img (slider - 1/ img - 0)
+    slider: 1,
+    campaignListTitle: 'CURRENT CAMPAIGNS', // title main page list campaign
+    videoPopupUrl: 'https://player.vimeo.com/video/198101157?title=0&amp;byline=0&amp;controls=0&fullscreen=1&buttons.share = false&embed.buttons.like = false;autoplay=0&portrait=0',
+    videoTitle: 'WHAT IS GROWTHFOUNTAIN?',
+    videoText: '<h3>We banded together with a simple mission: to simplify fundraising <br> and help businesses raise capital.</h3>'
+    + '<h3>Everybody in the world now has the ability to invest directly <br> in America’s entrepreneurs!</h3>',
+    dataVideoIdPopup: '198101157',
+    dataProviderVideoPopup: 'vimeo',
+    // top banner (none - 0 / dcu - 1 / river - 2)
+    topBanner: 0,
+    //bottom banner (1 - raise capital/ 0 - INVEST)
+    bottomBunner: 1,
+  },
+  currentSiteUrl: "growthfountain.com"
+};
